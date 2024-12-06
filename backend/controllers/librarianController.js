c67063cc6e778f96e88868077e0178be5e79ce57
raaiha-s <raaihasyed@gmail.com>
2024-12-06 23:54:00 +0500

    login tested

--- backend/controllers/librarianController.js
+++ backend/controllers/librarianController.js
@@ -1,50 +1,3 @@
-/*
-const Librarian = require('../models/librarians');
-
-// Get all librarians
-const getLibrarians = async (req, res) => {
-  try {
-    const librarians = await Librarian.find();
-    res.status(200).json(librarians);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Add a new librarian
-const addLibrarian = async (req, res) => {
-  const newLibrarian = new Librarian(req.body);
-  try {
-    const savedLibrarian = await newLibrarian.save();
-    res.status(201).json(savedLibrarian);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Update a librarian
-const updateLibrarian = async (req, res) => {
-  try {
-    const updatedLibrarian = await Librarian.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.status(200).json(updatedLibrarian);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Delete a librarian
-const deleteLibrarian = async (req, res) => {
-  try {
-    await Librarian.findByIdAndDelete(req.params.id);
-    res.status(200).json({ message: 'Librarian deleted!' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-module.exports = { getLibrarians, addLibrarian, updateLibrarian, deleteLibrarian };
-*/
-
 const Librarian = require('../models/librarians');
 
 // Middleware to check for admin role (can be reused across controllers)
@@ -65,7 +18,7 @@ const getLibrarians = async (req, res) => {
   }
 };
 
-// Add a new librarian
+//! Add a new librarian
 const addLibrarian = async (req, res) => {
   const newLibrarian = new Librarian(req.body);
   try {
@@ -101,4 +54,4 @@ const deleteLibrarian = async (req, res) => {
   }
 };
 
-module.exports = { getLibrarians, addLibrarian, updateLibrarian, deleteLibrarian };
+module.exports = { getLibrarians, addLibrarian, updateLibrarian, deleteLibrarian,verifyAdmin };
